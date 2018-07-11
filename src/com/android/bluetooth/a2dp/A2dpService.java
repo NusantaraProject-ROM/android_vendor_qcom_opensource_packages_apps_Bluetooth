c4e5d9940b8ae4fc125ba160d5fe0fc817a4ab76
Pavlin Radoslavov <pavlin@google.com>
2018-07-10 17:24:01 -0700

    Explicitly mute the audio output while switching the A2DP Active Device
    The mute/unmute of the audio output is needed during the A2DP
    ActiveDevice switch to avoid audio glitches.
    
    Bug: 78152025
    Bug: 110441865
    Test: Manual - stream with Pandora/Spotify and switch A2DP Active Device
    CRs-Fixed: 2301620
    Change-Id: I5a233460a07e0d4fffcb4e6cb208d846517f0c1e
    (cherry picked from commit 075005f0ef8adf5397b9ed6210e0160f1a0314a5)

--- src/com/android/bluetooth/a2dp/A2dpService.java
+++ src/com/android/bluetooth/a2dp/A2dpService.java
@@ -704,7 +704,14 @@ public class A2dpService extends ProfileService {
             }
             // Make sure the Audio Manager knows the previous Active device is disconnected,
             // and the new Active device is connected.
+            // Also, mute and unmute the output during the switch to avoid audio glitches.
+            boolean wasMuted = false;
             if (previousActiveDevice != null) {
+                if (!mAudioManager.isStreamMute(AudioManager.STREAM_MUSIC)) {
+                   mAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                                                 AudioManager.ADJUST_MUTE, 0);
+                   wasMuted = true;
+                }
                 if (mDummyDevice != null &&
                     mAdapterService.isTwsPlusDevice(previousActiveDevice)) {
                     mAudioManager.setBluetoothA2dpDeviceConnectionStateSuppressNoisyIntent(
@@ -763,6 +770,10 @@ public class A2dpService extends ProfileService {
                  SystemProperties.get("persist.vendor.btstack.enable.splita2dp");
             if (!(offloadSupported.isEmpty() || "true".equals(offloadSupported))) {
                 mAudioManager.handleBluetoothA2dpDeviceConfigChange(device);
+            }
+            if (wasMuted) {
+               mAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                                          AudioManager.ADJUST_UNMUTE, 0);
             }
             if(mAvrcp_ext != null)
                 mAvrcp_ext.setActiveDevice(device);
