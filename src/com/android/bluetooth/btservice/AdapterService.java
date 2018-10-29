17c11515ece500c43f701ad6cf72c86115c43df6
xiaowang <xiaowang@codeaurora.org>
2018-10-29 13:04:05 +0800

    Bluetooth: Enable aptx-hd/ldac codec according to chipset features
    Enable aptx-hd/ldac codec according to chipset support capabilities
    
    CRs-fixed: 2342576
    Change-Id: I824289f7b69ecec6723f2778c6f16c8fb1287d6d

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -2917,8 +2917,9 @@ public class AdapterService extends Service {
      * @return true if Split A2DP Source LDAC  is enabled
      */
     public boolean isSplitA2DPSourceLDAC() {
+        String BT_SOC = SystemProperties.get("vendor.bluetooth.soc");
         enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
-        return mAdapterProperties.isSplitA2DPSourceLDAC();
+        return BT_SOC.equals("cherokee") || mAdapterProperties.isSplitA2DPSourceLDAC();
     }
 
     /**
@@ -2937,8 +2938,9 @@ public class AdapterService extends Service {
      * @return true if Split A2DP Source APTX HD  is enabled
      */
     public boolean isSplitA2DPSourceAPTXHD() {
+        String BT_SOC = SystemProperties.get("vendor.bluetooth.soc");
         enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
-        return mAdapterProperties.isSplitA2DPSourceAPTXHD();
+        return BT_SOC.equals("cherokee") || mAdapterProperties.isSplitA2DPSourceAPTXHD();
     }
 
     /**
