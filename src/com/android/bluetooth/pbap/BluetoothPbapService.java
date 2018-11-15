5172960e03a0bcbf5925244028f2f5bd6e4be7b2
Sumit Deshmukh <sumitd@codeaurora.org>
2018-11-15 11:28:06 +0530

    PBAP: HandlerThread is terminated before all tasks are completed
    Usecase:
    1) Establish PBAP Connection.
    2) Turn Off Bluetooth.
    
    Issue:
    Tasks of Handler thread like closeService() are not
    completed on BT OFF. Like Bluetooth PBAP profile state
    (DISCONNECTED) is not broadcasted on BT OFF.
    
    Reproducible Rate: 100%.
    
    Root Cause:
    HandlerThread associated with PbapHandler is terminated using
    quitSafely() which causes subsequent tasks not getting handled.
    As PbapStateMachine uses same HandlerThread, DISCONNECT message
    sent in closeService() is not handled and thus profile state is
    not broadcasted.
    This bug is introduced in P through  MultiPbap commit
    a08d7327b23d2c2192c2ab9f010d6f494c11699b.
    
    Fix:
    Terminate HandlerThread in closeService() once all messages of
    Handler are processed and PBAP Connection state is broadcasted
    from PbapStateMachine.
    
    Test: Sanity testing with BT on/off is done.
    Change-Id: I04e6a40836d78f8113b906994fb78dc64748d710
    CRs-Fixed: 2350747

--- src/com/android/bluetooth/pbap/BluetoothPbapService.java
+++ src/com/android/bluetooth/pbap/BluetoothPbapService.java
@@ -131,9 +131,11 @@ public class BluetoothPbapService extends ProfileService implements IObexConnect
     static final int CHECK_SECONDARY_VERSION_COUNTER = 6;
     static final int ROLLOVER_COUNTERS = 7;
     static final int GET_LOCAL_TELEPHONY_DETAILS = 8;
+    static final int CLEANUP_HANDLER_TASKS = 9;
 
     static final int USER_CONFIRM_TIMEOUT_VALUE = 30000;
     static final int RELEASE_WAKE_LOCK_DELAY = 10000;
+    static final int CLEANUP_HANDLER_DELAY = 50;
 
     private PowerManager.WakeLock mWakeLock;
 
@@ -324,6 +326,16 @@ public class BluetoothPbapService extends ProfileService implements IObexConnect
         if (mSessionStatusHandler != null) {
             mSessionStatusHandler.removeCallbacksAndMessages(null);
         }
+
+        mSessionStatusHandler.sendMessageDelayed(
+            mSessionStatusHandler.obtainMessage(CLEANUP_HANDLER_TASKS), CLEANUP_HANDLER_DELAY);
+    }
+
+    private void cleanUpHandlerTasks() {
+        Log.d(TAG, "quitHandlerThread");
+        if (mHandlerThread != null) {
+            mHandlerThread.quitSafely();
+        }
     }
 
     private void cleanUpServerSocket() {
@@ -451,6 +463,10 @@ public class BluetoothPbapService extends ProfileService implements IObexConnect
                     break;
                 case GET_LOCAL_TELEPHONY_DETAILS:
                     getLocalTelephonyDetails();
+                    break;
+                case CLEANUP_HANDLER_TASKS:
+                    cleanUpHandlerTasks();
+                    break;
                 default:
                     break;
             }
@@ -570,9 +586,6 @@ public class BluetoothPbapService extends ProfileService implements IObexConnect
         setBluetoothPbapService(null);
         if (mSessionStatusHandler != null) {
             mSessionStatusHandler.obtainMessage(SHUTDOWN).sendToTarget();
-        }
-        if (mHandlerThread != null) {
-            mHandlerThread.quitSafely();
         }
         mContactsLoaded = false;
         if (mContactChangeObserver == null) {
