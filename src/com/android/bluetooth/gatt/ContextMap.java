7732e790c9ce37c748aea1999906118aadf72546
Eugene Susla <eugenesusla@google.com>
2019-07-24 17:24:17 -0700

    [CDM] Bypass location setting when scanning for devices
    Test: turn off location in settings and ensure devices still shown in UI
    Change-Id: Id199a1c36af882558450d1d0a2bf1de5f745b816
    (cherry picked from commit d1f44b2d2c663e6157c3511e6606acce0ab920ec)

--- src/com/android/bluetooth/gatt/ContextMap.java
+++ src/com/android/bluetooth/gatt/ContextMap.java
@@ -106,8 +106,12 @@ import java.util.UUID;
         /** Whether the calling app has the network setup wizard permission */
         boolean mHasNetworkSetupWizardPermission;
 
-        public List<String> mAssociatedDevices;
+        /** Whether the calling app has the network setup wizard permission */
+        boolean mHasScanWithoutLocationPermission;
+
         boolean mEligibleForSanitizedExposureNotification;
+
+        public List<String> mAssociatedDevices;
 
         /** Internal callback info queue, waiting to be send on congestion clear */
         private List<CallbackInfo> mCongestionQueue = new ArrayList<CallbackInfo>();
