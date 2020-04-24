50f0da04e5e2af7a14388269a24a506d57fd519a
Rahul Samana <rsamana@codeaurora.org>
2020-04-24 15:37:33 +0800

    NIAP: implement bluetooth keystore interface.(1/2)
    Bug: 148758680
    Test: m
    Change-Id: I781d52b8a5b7cc6c02f8589072a37b3214ab6d0b
    Merged-In: I781d52b8a5b7cc6c02f8589072a37b3214ab6d0b
    (cherry picked from commit 4feda407be24ca96f7fe92110bac2152dcafdceb)

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -599,6 +599,8 @@ public class AdapterService extends Service {
         mBatteryStats = IBatteryStats.Stub.asInterface(
                 ServiceManager.getService(BatteryStats.SERVICE_NAME));
 
+        mBluetoothKeystoreService.initJni();
+
         mSdpManager = SdpManager.init(this);
         registerReceiver(mAlarmBroadcastReceiver, new IntentFilter(ACTION_ALARM_WAKEUP));
         IntentFilter wifiFilter = new IntentFilter();
