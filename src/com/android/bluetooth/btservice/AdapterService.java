738eb3e45d1f79728499bf8906af878a0d097310
Venkata Jagadeesh Garaga <vgaraga@codeaurora.org>
2020-12-21 15:57:03 +0530

    Revert "Revert "Bluetooth: Use public system API to get Band for the AP.""
    This reverts commit 995c67af596cd91a772774f6d72622ee8bed4280.
    
    Change-Id: I09dcb59516803f227c0bbf9892e0576c0d5405fe
    CRs-Fixed: 2866059

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -139,7 +139,7 @@ import com.android.internal.util.ArrayUtils;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiConfiguration;
+import android.net.wifi.SoftApConfiguration;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 
@@ -4336,10 +4336,10 @@ public class AdapterService extends Service {
         try {
 
             WifiManager mWifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
-            final WifiConfiguration config = mWifiManager.getWifiApConfiguration();
+            final SoftApConfiguration config = mWifiManager.getSoftApConfiguration();
             if ((mWifiManager != null) && ((mWifiManager.isWifiEnabled() ||
                 ((mWifiManager.getWifiApState() == WifiManager.WIFI_AP_STATE_ENABLED) &&
-                (config.apBand == WifiConfiguration.AP_BAND_5GHZ))))) {
+                (config.getBand() == SoftApConfiguration.BAND_5GHZ))))) {
                 return true;
             }
             return false;
