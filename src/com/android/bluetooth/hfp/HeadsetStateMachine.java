5aaee3391dc4da5ac7d9affead9b45b94203a7cf
Satish Kodishala <skodisha@codeaurora.org>
2021-03-09 19:22:05 +0530

    Bluetooth: Send SCO immediately after SLC for few remotes
    Send SCO immediately after SLC for few remotes
    which need it if there is an ongoing call on
    phone.
    
    CRs-Fixed: 2855941
    Change-Id: I8240dffaa3f810d40f5c355cde01ab7b74367a0e

--- src/com/android/bluetooth/hfp/HeadsetStateMachine.java
+++ src/com/android/bluetooth/hfp/HeadsetStateMachine.java
@@ -212,6 +212,7 @@ public class HeadsetStateMachine extends StateMachine {
     private boolean mPendingScoForVR = false;
     private boolean mIsCallIndDelay = false;
     private boolean mIsBlacklistedDevice = false;
+    private boolean mIsBlacklistedForSCOAfterSLC = false;
     private int retryConnectCount = 0;
 
     private static boolean mIsAvailable = false;
@@ -373,6 +374,12 @@ public class HeadsetStateMachine extends StateMachine {
             ProfileService.println(sb, "    " + line);
         }
         scanner.close();
+    }
+
+    public boolean getIfDeviceBlacklistedForSCOAfterSLC() {
+        Log.d(TAG, "getIfDeviceBlacklistedForSCOAfterSLC, returning " +
+                             mIsBlacklistedForSCOAfterSLC);
+        return  mIsBlacklistedForSCOAfterSLC;
     }
 
     /**
@@ -1346,6 +1353,8 @@ public class HeadsetStateMachine extends StateMachine {
                 sendMessageDelayed(QUERY_PHONE_STATE_AT_SLC, QUERY_PHONE_STATE_CHANGED_DELAYED);
                 // Checking for the Blacklisted device Addresses
                 mIsBlacklistedDevice = isConnectedDeviceBlacklistedforIncomingCall();
+                // Checking for the Blacklisted device Addresses
+                mIsBlacklistedForSCOAfterSLC = isSCONeededImmediatelyAfterSLC();
                 if (mSystemInterface.isInCall() || mSystemInterface.isRinging()) {
                    stateLogW("Connected: enter: suspending A2DP for Call since SLC connected");
                    // suspend A2DP since call is there
@@ -2928,6 +2937,14 @@ public class HeadsetStateMachine extends StateMachine {
     boolean isDeviceBlacklistedForSendingCallIndsBackToBack() {
         boolean matched = InteropUtil.interopMatchAddrOrName(
             InteropUtil.InteropFeature.INTEROP_HFP_SEND_CALL_INDICATORS_BACK_TO_BACK,
+            mDevice.getAddress());
+
+        return matched;
+    }
+
+    boolean isSCONeededImmediatelyAfterSLC() {
+        boolean matched = InteropUtil.interopMatchAddrOrName(
+            InteropUtil.InteropFeature.INTEROP_SETUP_SCO_WITH_NO_DELAY_AFTER_SLC_DURING_CALL,
             mDevice.getAddress());
 
         return matched;
