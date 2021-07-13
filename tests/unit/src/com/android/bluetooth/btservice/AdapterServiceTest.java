LineageOS/android_packages_apps_Bluetooth

Common Criteria mode: Rename Common Criteria mode from NIAP mode. (1/2)
Bug: 181293964
Test: atest BluetoothInstrumentationTests
Tag: #feature
Change-Id: Iab4fa1b65dfeba7e205f3eeb496ce95473c587e8

tests/unit/src/com/android/bluetooth/btservice/AdapterServiceTest.java
@@ -108,7 +108,7 @@ public class AdapterServiceTest {
         }
         Assert.assertNotNull(Looper.myLooper());
         AdapterService adapterService = new AdapterService();
-        adapterService.initNative(false /* is_restricted */, false /* is_niap_mode */,
+        adapterService.initNative(false /* is_restricted */, false /* is_common_criteria_mode */,
                 0 /* config_compare_result */, new String[0], false);
         adapterService.cleanupNative();
         HashMap<String, HashMap<String, String>> adapterConfig = TestUtils.readAdapterConfig();

NusantaraProject-ROM/android_vendor_qcom_opensource_packages_apps_Bluetooth

(cherry picked from commit 7398f198a05c1c255623bd9bfc0d376df67256e1)

tests/unit/src/com/android/bluetooth/btservice/AdapterServiceTest.java
@@ -104,7 +104,7 @@ public class AdapterServiceTest {
         }
         Assert.assertNotNull(Looper.myLooper());
         AdapterService adapterService = new AdapterService();
-        adapterService.initNative(false /* is_restricted */, false /* is_niap_mode */,
+        adapterService.initNative(false /* is_restricted */, false /* is_common_criteria_mod */,
                  0 /* config_compare_result */, false,
                  new String[0]);
         adapterService.cleanupNative();